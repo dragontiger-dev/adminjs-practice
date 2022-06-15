5f9635b430c8991dc838eb8bdd0150e9cae9905b
tiger <tiger@noncelab.com>
2022-06-15 14:03:30 +0900

    스키마 추가 연습 (users)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,36 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from '@adminjs/nestjs';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
+import { DMMFClass } from '@prisma/client/runtime';
+import AdminJS from 'adminjs';
+import { Database, Resource } from '@adminjs/prisma';
+
+AdminJS.registerAdapter({ Resource, Database });
 
 @Module({
 	imports: [
-		AdminModule.createAdmin({
-			adminJsOptions: {
-				rootPath: '/',
-				resources: [],
+		PrismaModule,
+		AdminModule.createAdminAsync({
+			imports: [PrismaModule],
+			inject: [PrismaService],
+			useFactory: async (prisma: PrismaService) => {
+				const dmmf = (prisma as any)._dmmf as DMMFClass;
+				return {
+					adminJsOptions: {
+						rootPath: '/',
+						resources: [
+							{
+								resource: {
+									model: dmmf.modelMap.users,
+									client: prisma,
+								},
+								options: {},
+							},
+						],
+					},
+				};
 			},
 		}),
 	],
